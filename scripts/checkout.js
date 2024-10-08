77c29c426d80f2e6391833e0203fa609e760c77a
Krittika <krittikabisht019@gmail.com>
2024-10-08 22:59:23 +0530

    Added the removed css files

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,5 @@
 import { renderorderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/cart-class.js' ;
+//import '../data/cart-class.js' ;
 renderorderSummary() ;
 renderPaymentSummary() ;
